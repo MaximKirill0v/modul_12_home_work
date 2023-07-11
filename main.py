ac85e2164395578c53c0306e773586f67d93f949
Maxim Kirillov <maximkir181210@gmail.com>
2023-07-12 00:01:14 +0300

    Academy поправки

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-import sqlite3
-
-
-def create_tuple():
-    list_tuple = []
-    count = 1
-    for group in range(1, 9):
-        for _ in range(1, 11):
-            list_tuple.append((group, count))
-            count += 1
-    return list_tuple
-
-
 def execute_application():
-    a = create_tuple()
-    db_name =
-    connection = sqlite3.Connection()
-    cursor = connection.cursor()
-
+    pass
 
 
 if __name__ == '__main__':
