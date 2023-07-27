796c9cb26a405d97f364e6c2efd707503006ad20
Maxim Kirillov <maximkir181210@gmail.com>
2023-07-27 22:52:15 +0300

    DB People start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 import os
+import json
 
 
 # Задание 1.
@@ -111,6 +112,66 @@ def delete_data_from_db(name_db: str, connection, name_table, field_to_delete: s
             print(f'БД {name_db} не существует.')
 
 
+def update_phone(name_db: str, connection, name_table, field_to_update: str, new_values: str, last_values: str):
+    with connection:
+        if os.path.exists(name_db):
+            cursor = connection.cursor()
+            try:
+                sql_request = f'''UPDATE {name_table}
+                                  SET {field_to_update} = ?
+                                  WHERE {field_to_update} = ?
+                               '''
+                cursor.execute(sql_request, (new_values, last_values))
+                print(f'Старые данные - "{last_values}" успешно заменены на "{new_values}".')
+            except Error as e:
+                print(e)
+            finally:
+                connection.commit()
+                cursor.close()
+        else:
+            print(f'БД {name_db} не существует.')
+
+
+def save_to_json_file(path: str):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            fields = func(*args, **kwargs)
+            with open(path, 'a') as file:
+                print(fields)
+                for field in fields:
+                    json.dump(field, file)
+            return fields
+
+        return wrapper
+
+    return decorator
+
+
+@save_to_json_file('./people_data/data_people.json')
+def filter_by_city(name_db: str, connection, name_table, fields: str, values: str):
+    if os.path.exists(name_db):
+        cursor = connection.cursor()
+        try:
+            sql_request = f'''SELECT * FROM {name_table}
+                              WHERE {fields} = ? 
+                           '''
+            cursor.execute(sql_request, (values,))
+            fields = cursor.fetchall()
+            return fields
+        except Error as e:
+            print(e)
+        finally:
+            cursor.close()
+    else:
+        print(f'БД {name_db} не существует.')
+
+
+def interface_app():
+    print('Введите число для работы с приложением: \n=> 1 - добавить данные пользователя в БД'
+          '\n=> 2 - изменение данных в БД\n=> 3 - выборка данных пользователей по фильтру\n'
+          '=> 4 - удаление данных пользователя из таблицы\n=> 5 - выход из программы')
+
+
 def execute_application():
     name_db = 'People.db'
     connection = sqlite3.connect(name_db)
@@ -118,24 +179,76 @@ def execute_application():
     create_table(connection, name_table)
 
     # добавление в таблицу данных одного человека
-    people_data = ('Maxim', 'Kirillov', 'Russia', 'Yaroslavl', 'Lenina', '1989-02-14', '+671-0795-532-256')
-    insert_one_people(name_db, connection, name_table, people_data)
+    # people_data = ('Maxim', 'Kirillov', 'Russia', 'Yaroslavl', 'Lenina', '1989-02-14', '+671-0795-532-256')
+    # insert_one_people(name_db, connection, name_table, people_data)
 
     # считывание данных из txt файла и преобразование данных к списку кортежей
-    path_to_txt_people_data = './people_data/data_people.txt'
-    people_list_tuple = read_people_data_from_txt(path_to_txt_people_data)
+    # path_to_txt_people_data = './people_data/data_people.txt'
+    # people_list_tuple = read_people_data_from_txt(path_to_txt_people_data)
 
     # добавление в таблицу данных многих людей
-    insert_many_people(name_db, connection, name_table, people_list_tuple)
+    # insert_many_people(name_db, connection, name_table, people_list_tuple)
 
-    # удаление данных из таблицы по номеру введённому полю и его значению
-    values_to_delete = '+350-3833-535-675'
-    fields_for_delete = 'phone'
-    if not check_values_from_db(connection, name_db, name_table, fields_for_delete, values_to_delete):
-        delete_data_from_db(name_db, connection, name_table, fields_for_delete, values_to_delete)
-    else:
-        print(f'Человек с полем {fields_for_delete} = {values_to_delete} '
-              f'отсутствует в таблице. Удаление данных не возможно.')
+    # удаление данных из таблицы по введённому полю и его значению
+    # fields_for_delete = 'phone'
+    # values_to_delete = '+350-3833-535-675'
+    #
+    # if not check_values_from_db(connection, name_db, name_table, fields_for_delete, values_to_delete):
+    #     delete_data_from_db(name_db, connection, name_table, fields_for_delete, values_to_delete)
+    # else:
+    #     print(f'Человек с полем {fields_for_delete} = {values_to_delete} '
+    #           f'отсутствует в таблице. Удаление данных не возможно.')
+
+    # изменение данных в БД
+    # last_values = '+65-2061-655-061'
+    #     # new_values = '+43-9021-344-097'
+    #     # fields_for_update = 'phone'
+    # if not check_values_from_db(connection, name_db, name_table, fields_for_update, last_values):
+    #     update_phone(name_db, connection, name_table, fields_for_update, new_values, last_values)
+    # else:
+    #     print(f'Человек с полем {fields_for_update} = {last_values} '
+    #           f'отсутствует в таблице. Обновление данных не возможно.')
+
+    # выборка из БД
+    # field = 'country'
+    # values = 'Nigeria'
+    #
+    # fields = filter_by_city(name_db, connection, name_table, field, values)
+    # if fields:
+    #     for field in fields:
+    #         print(field)
+    # else:
+    #     print(f"Данные с полем '{field}' = '{values}' в БД не найдены.")
+
+    while True:
+        interface_app()
+        number_input = input('=> ')
+        numbers_app = ('1', '2', '3', '4', '5')
+        if number_input == '1':
+            print('Введите имя, фамилию, страну, город, улицу, дату рождения, телефон через запятую и пробел:\n'
+                  '# Степанов, Степан, Россия, Саратов, ул. Ленина, 1999-12-28, +671-0795-532-200')
+            people_data = tuple(input().split(', '))
+            try:
+                insert_one_people(name_db, connection, name_table, people_data)
+                print('Пользователь успешно добавлен в БД.')
+            except Error as e:
+                print(e)
+            else:
+                print('Не корректные данные, повторите ввод.')
+
+        if number_input == '2':
+            
+            fields_for_update = 'name', 'surname', 'country', 'city', 'address', 'date_of_birth', 'phone'
+
+            field_for_update = input('Введите название поля, значение которого хотите изменить'
+                                     '(name, surname, country, city, address, date_of_birth, phone): ')
+            if field_for_update in fields_for_update:
+
+
+        if number_input not in numbers_app:
+            print('Введено не корректное число, повторите попытку.')
+        if number_input == '5':
+            break
 
     connection.close()
     print(f'Соединение с БД "{name_db}" завершено.')
